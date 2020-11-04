ac026f83ee075ee6b63c5e0b657d639fb88b226d
kotser1 <59193071+kotser1@users.noreply.github.com>
2020-11-04 15:29:56 +0300

    Правки по критериям
    - правки по критериям
    - префиксы в css
    - замена .png-картинок на .svg в разделе Брэнды
    - добавлено переключение слайдов
    -  подключены минифиц. версии css и js файлов.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Форма обратной связи
+
 const popupFeedback = document.querySelector('.modal-feedback');
 const openFeedbackButton = document.querySelector('.contacts-button');
 const closePopupButton = popupFeedback.querySelector('.modal-close');
@@ -9,7 +11,6 @@ const feedbackMessage = document.querySelector('.feedback-message-field');
 let isStorageSupport = true;
 let storageName = "";
 let storageEmail = "";
-
 
 try {
   storageName = localStorage.getItem("Name");
@@ -25,12 +26,10 @@ openFeedbackButton.addEventListener('click', function (evt) {
   if (storageName) {
     feedbackName.value = storageName;
     feedbackEmail.focus();
-
     if (storageEmail) {
       feedbackEmail.value = storageEmail;
       feedbackMessage.focus();
     }
-
   } else {
     feedbackName.focus();
   }
@@ -39,7 +38,7 @@ openFeedbackButton.addEventListener('click', function (evt) {
 closePopupButton.addEventListener('click', function () {
   popupFeedback.classList.remove('modal-show');
   popupFeedback.classList.remove('modal-error');
-})
+});
 
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
@@ -81,4 +80,73 @@ document.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     popupMap.classList.remove('modal-show');
   }
+});
+
+
+//Переключение вкладок в разделе "Сервисы"
+
+const deliveryButton = document.querySelector('.delivery-button');
+const warrantyButton = document.querySelector('.warranty-button');
+const creditButton = document.querySelector('.credit-button');
+const deliveryContent = document.querySelector('.delivery-item');
+const warrantyContent = document.querySelector('.warranty-item');
+const creditContent = document.querySelector('.credit-item');
+
+deliveryButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  warrantyButton.classList.remove('services-button-current');
+  creditButton.classList.remove('services-button-current');
+  deliveryButton.classList.add('services-button-current');
+
+  warrantyContent.classList.remove('services-item-current');
+  creditContent.classList.remove('services-item-current');
+  deliveryContent.classList.add('services-item-current');
+});
+
+warrantyButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  deliveryButton.classList.remove('services-button-current');
+  creditButton.classList.remove('services-button-current');
+  warrantyButton.classList.add('services-button-current');
+
+  deliveryContent.classList.remove('services-item-current');
+  creditContent.classList.remove('services-item-current');
+  warrantyContent.classList.add('services-item-current');
+});
+
+creditButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  deliveryButton.classList.remove('services-button-current');
+  warrantyButton.classList.remove('services-button-current');
+  creditButton.classList.add('services-button-current');
+
+  deliveryContent.classList.remove('services-item-current');
+  warrantyContent.classList.remove('services-item-current');
+  creditContent.classList.add('services-item-current');
+});
+
+
+//Слайдер
+
+const slideFirst = document.querySelector('.slide-first');
+const slideSecond = document.querySelector('.slide-second');
+const buttonBack = document.querySelector('.slider-button-back');
+const buttonNext = document.querySelector('.slider-button-next');
+const controlFirst = document.querySelector('.control-first');
+const controlSecond = document.querySelector('.control-second');
+
+buttonNext.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  slideFirst.classList.toggle('slide-current');
+  slideSecond.classList.toggle('slide-current');
+  controlFirst.classList.toggle('slider-control-current');
+  controlSecond.classList.toggle('slider-control-current');
+});
+
+buttonBack.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  slideFirst.classList.toggle('slide-current');
+  slideSecond.classList.toggle('slide-current');
+  controlFirst.classList.toggle('slider-control-current');
+  controlSecond.classList.toggle('slider-control-current');
 });
